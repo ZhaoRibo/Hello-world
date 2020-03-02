5a6132e991153d1da60b5214b76a591266597459
ZhaoRuibo's Mac <zhaoruibo@zhaoruibodeMac.mshome.net>
2020-03-03 01:07:17 +0800

    First Commit

---
+++ Hello World/Hello World/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Hello World
+//
+//  Created by 赵瑞博 on 2/3/2020.
+//  Copyright © 2020 赵瑞博. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .foregroundColor(Color.red)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Hello World/Hello WorldTests/Hello_WorldTests.swift
@@ -0,0 +1,34 @@
+//
+//  Hello_WorldTests.swift
+//  Hello WorldTests
+//
+//  Created by 赵瑞博 on 2/3/2020.
+//  Copyright © 2020 赵瑞博. All rights reserved.
+//
+
+import XCTest
+@testable import Hello_World
+
+class Hello_WorldTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
